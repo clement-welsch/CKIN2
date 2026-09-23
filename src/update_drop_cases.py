e77f6e68b49c804b6791527f24218b7a103b80ef
clement <welsch.clement@gmail.com>
2026-09-23 16:19:07 +0200

    get the new price of one case

--- src/update_drop_cases.py
+++ src/update_drop_cases.py
@@ -1,4 +1,9 @@
+import pandas as pd
 import requests
+from pathlib import Path
+
+#ENVIRONMENT = "home"
+ENVIRONMENT = "school"
 
 def connect_to_steam(name_case):
     appid = 730
@@ -16,9 +21,16 @@ def connect_to_steam(name_case):
     response = requests.get(url, params=params, timeout=10)
     print(response.status_code)
     print(response.text)
+    return response.json()
 
-def get_price_for_case(name_case):
-    connect_to_steam(name_case)
-    return 0
+def get_price_for_case(case_name):
+    if ENVIRONMENT == "home":
+        return connect_to_steam(case_name)
+    else:
+        df=pd.read_csv("./data/processed/drop_cases_prices.csv")
+        price = df[df["case_name"] == case_name].tail(1)["unit_price"].iloc[0]
+        print(price)
+        return price
 
-get_price_for_case("Dreams & Nightmares")
+new_price = get_price_for_case("Dreams & Nightmares")
+print(new_price)
